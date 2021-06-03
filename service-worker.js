b571a79fa798106e09648ec7c7873890d7ef0d18
Majoramari <muhannadexz@gmail.com>
2021-06-03 06:11:14 +0200

    'build'

--- service-worker.js
+++ service-worker.js
@@ -21,11 +21,11 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "d2a73dd65ecd39c92b9fbf565229aa42"
+    "revision": "f86fd3a0a69af837ad0fe7b918063619"
   },
   {
-    "url": "assets/css/0.styles.4c3d2431.css",
-    "revision": "bfbb7196e922dbf83fd9b55ec4769a91"
+    "url": "assets/css/0.styles.57818cda.css",
+    "revision": "e6b8e0bce448b447abae1feb4351e686"
   },
   {
     "url": "assets/fonts/element-icons.535877f5.woff",
@@ -36,60 +36,60 @@ self.__precacheManifest = [
     "revision": "732389ded34cb9c52dd88271f1345af9"
   },
   {
-    "url": "assets/js/10.1a2e59a0.js",
-    "revision": "f121aff2e3d06604b84e081054e830a0"
+    "url": "assets/js/10.293bf025.js",
+    "revision": "f5f90ded0f47abfb41baca38e1133ac0"
   },
   {
-    "url": "assets/js/11.8ca1d78e.js",
+    "url": "assets/js/11.a55b617a.js",
     "revision": "f5e5b20755b750c4e4728726dca30299"
   },
   {
-    "url": "assets/js/12.987b1abb.js",
-    "revision": "5e170f4543e3e90cb29b5b4e8ef0ecb1"
+    "url": "assets/js/12.8b0c6758.js",
+    "revision": "94c0fd83a85148d0add3d2757b58b771"
   },
   {
-    "url": "assets/js/13.0be4541f.js",
-    "revision": "bb81d484c2254fe3aede392d67d90400"
+    "url": "assets/js/13.a4ad73be.js",
+    "revision": "c22274bd421693a084e354c0d58ccd63"
   },
   {
-    "url": "assets/js/14.756e14c0.js",
+    "url": "assets/js/14.ef84118e.js",
     "revision": "52d85cf98d76c9a7200c69f986c03d7f"
   },
   {
-    "url": "assets/js/15.828c8b4c.js",
-    "revision": "b32402b389208ed55b16f112d0f5e1de"
+    "url": "assets/js/15.c3a90b9b.js",
+    "revision": "a9d78fcad88a22c4cf3128c722d15512"
   },
   {
-    "url": "assets/js/3.35007d4e.js",
+    "url": "assets/js/3.f48aba4e.js",
     "revision": "26aa99606774eec0ccd8b0039f2d4702"
   },
   {
-    "url": "assets/js/4.169470d0.js",
+    "url": "assets/js/4.cc810973.js",
     "revision": "300907837bbf73e02ae40dcf271df5d8"
   },
   {
-    "url": "assets/js/5.12d6c576.js",
+    "url": "assets/js/5.47503dba.js",
     "revision": "122aab989d62428fff8578d6a56148e6"
   },
   {
-    "url": "assets/js/6.7172a159.js",
+    "url": "assets/js/6.206c79d1.js",
     "revision": "d3fcfeec9596e0c7ce67b6056a1ba17e"
   },
   {
-    "url": "assets/js/7.0992cad6.js",
+    "url": "assets/js/7.b44aa754.js",
     "revision": "c8e42d7c8beda1ca0d243bb37dcd00da"
   },
   {
-    "url": "assets/js/8.809d0053.js",
+    "url": "assets/js/8.d0a25fbd.js",
     "revision": "d496b7237cdc61b16551e214c72513e7"
   },
   {
-    "url": "assets/js/9.689922f4.js",
+    "url": "assets/js/9.480ef8d6.js",
     "revision": "a7f0558698cf3c328df1dd7e4a4d1b31"
   },
   {
-    "url": "assets/js/app.446744ce.js",
-    "revision": "6d8746eebd9b1bd9bb92dab5350d9c7f"
+    "url": "assets/js/app.a3508997.js",
+    "revision": "a132787b3644feca9c49edf40990cd84"
   },
   {
     "url": "assets/js/vuejs-paginate.3d07f62b.js",
@@ -97,31 +97,31 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "76965117bca7d42cc62cdc0054eeab67"
+    "revision": "99f5474951c868c3e651b70956d08a8b"
   },
   {
     "url": "post/how-to-join-mc/index.html",
-    "revision": "c0e2967b6ab8b189edbdf826e9a6929a"
+    "revision": "a0ae5f02615b6a88b09ceb4b93748572"
   },
   {
     "url": "projects/index.html",
-    "revision": "e6de25ea79f2e2ede51ce1056f8e362e"
+    "revision": "70965352531d8a109c921de13936bfdc"
   },
   {
-    "url": "projects/project/index.html",
-    "revision": "de3a490c583fcc45f2d32a7784a1711d"
+    "url": "projects/serqet-bot/index.html",
+    "revision": "916d36eb7a1becd4c6a17910fea3b3e3"
   },
   {
     "url": "tag/index.html",
-    "revision": "b489a7f9ad224b5123f269adde043d97"
+    "revision": "6d21d88575e0c65ea737d453094d4a4f"
   },
   {
     "url": "tag/ديسكورد/index.html",
-    "revision": "b1620b7880ffc2e2df0a8654c1180c6b"
+    "revision": "5d7043e22d0e3c16b93d27ab71366fc0"
   },
   {
     "url": "tag/ماين كرافت/index.html",
-    "revision": "f7ecfc35b38e5fbdf8642d12cd4929ab"
+    "revision": "2d18938d9ef92d7d15c2a5e1c6ea8021"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
